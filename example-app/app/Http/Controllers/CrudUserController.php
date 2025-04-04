31550d88605738ecdd2194b60ef68bea3fdaf3bb
JushLeHuuTin <jushlehuutinads@gmail.com>
2025-04-04 15:03:57 +0700

    thoi gian bat dau

--- example-app/app/Http/Controllers/CrudUserController.php
+++ example-app/app/Http/Controllers/CrudUserController.php
@@ -60,7 +60,6 @@ class CrudUserController extends Controller
     {
         $request->validate([
             'username' => 'required|unique:users',
-            // 'phone' => 'required',
             // 'address' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
@@ -70,7 +69,6 @@ class CrudUserController extends Controller
         $data = $request->all();
         $check = User::create([
             'username' => $data['username'],
-            // 'phone' => $data['phone'],
             // 'address' => $data['address'],
             'email' => $data['email'],
             'password' => FacadesHash::make($data['password'])
